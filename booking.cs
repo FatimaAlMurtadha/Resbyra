b4668242260225faeeae81ce4ed1d910660510af
arbazshah52 <arbazexample.com>
2025-12-08 15:05:21 +0100

    put booking

--- booking.cs
+++ booking.cs
@@ -15,6 +15,8 @@ public record Post_Args(decimal TotalPrice, DateTime Date, int UserId, int Packa
 // Booking update
 public record Put_Args(int Id, decimal TotalPrice, DateTime Date, int UserId, int PackageId);
 }
+
+//GET All
 public static async Task<List<GetAll_Data>> GetAll(Config config)
 {
     List<GetAll_Data> result = new();
@@ -32,6 +34,7 @@ public static async Task<List<GetAll_Data>> GetAll(Config config)
     }
     return result;
 }
+//GET Booking ID
 public static async Task<IResult> Get(int id, Config config)
 {
     Get_Data? result = null;
@@ -42,6 +45,34 @@ public static async Task<IResult> Get(int id, Config config)
         result = new(reader.GetDecimal(0), reader.GetDateTime(1), reader.GetInt32(2), reader.GetInt32(3));
 
     return result is null ? Results.NotFound(new { message = $"Booking {id} not found." }) : Results.Ok(result);
+}
+
+//Post Booking
+public static async Task Post(Post_Args args, Config config)
+{
+    string query = "INSERT INTO bookings(total_price, date, user_id, package_id) VALUES(@total_price, @date, @user_id, @package_id)";
+    var parameters = new MySqlParameter[]
+    {
+        new("@total_price", args.TotalPrice),
+        new("@date", args.Date),
+        new("@user_id", args.UserId),
+        new("@package_id", args.PackageId)
+    };
+    await MySqlHelper.ExecuteNonQueryAsync(config.ConnectionString, query, parameters);
+}
+//Put
+public static async Task Put(Put_Args args, Config config)
+{
+    string query = "UPDATE bookings SET total_price=@total_price, date=@date, user_id=@user_id, package_id=@package_id WHERE id=@id";
+    var parameters = new MySqlParameter[]
+    {
+        new("@id", args.Id),
+        new("@total_price", args.TotalPrice),
+        new("@date", args.Date),
+        new("@user_id", args.UserId),
+        new("@package_id", args.PackageId)
+    };
+    await MySqlHelper.ExecuteNonQueryAsync(config.ConnectionString, query, parameters);
 }
 
 
