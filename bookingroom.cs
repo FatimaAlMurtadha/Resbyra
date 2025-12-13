75239f79c4cc4c8be7a95191bd4784b03a5e3400
arbazshah52 <arbazexample.com>
2025-12-13 14:00:42 +0100

    post

--- bookingroom.cs
+++ bookingroom.cs
@@ -36,4 +36,33 @@ class BookRoom
       }
 }
 //GET bookroom id
+public static async Task<IResult> Get(int id, Config config)
+    {
+
+        string query = @"SELECT user_id, room_id, check_in, check_out, guests, total_price 
+                         FROM bookroom WHERE id = @id";
+
+       var parameters = new MySqlParameter[]
+        {
+            new("@id", id)
+        };
+using var reader = await MySqlHelper.ExecuteReaderAsync(config.ConnectionString, query, parameters);
+
+        if (reader.Read())
+        {
+            result = new(
+            
+                reader.GetInt32("user_id"),
+                reader.GetInt32("room_id"),
+                reader.GetDateTime("check_in"),
+                reader.GetDateTime("check_out"),
+                reader.GetInt32("guests"),
+                reader.GetDecimal("total_price")
+            );
+            return Results.NotFound(new { message = $"Booking with id {id} was not found." });
+        
+        }
+    }
+}
+//Post 
 
