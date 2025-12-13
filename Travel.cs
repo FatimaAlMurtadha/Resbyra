0595c5e4e14ed112601cf6c3f02d6271501fa123
arbazshah52 <arbazexample.com>
2025-12-13 22:32:05 +0100

    Create

--- Travel.cs
+++ Travel.cs
@@ -5,5 +5,59 @@ using Microsoft.AspNetCore.Http;
 
 class Travelers
 {
- public record GetAll_Data(int Id,string FirstName,string LastName,string PassportNumber,int Age,int BookingId);
- 
+ public record GetAll_Data(int Id,string FirstName,string LastName,string PassportNumber,int Age,int BookingId,int UserId);
+ //GET All
+ public static async Task<IResult> GetAll(Config config)
+{
+        List<GetAll_Data> result = new();
+string query = """
+            SELECT id, first_name, last_name, passport_number, age, booking_id, user_id
+            FROM travelers
+        """;
+using var reader =
+await MySqlHelper.ExecuteReaderAsync(config.ConnectionString, query);
+ while (reader.Read())
+       {
+            result.Add(new(
+                reader.GetInt32("id"),
+                reader.GetString("first_name"),
+                reader.GetString("last_name"),
+                reader.GetString("passport_number"),
+                reader.GetInt32("age"),
+                reader.GetInt32("booking_id"),
+                 reader.GetInt32("user_id")
+            ));
+        }
+        return Results.Ok(result);
+    }
+//Get BY ID
+public static async Task<IResult> Get(int id, Config config)
+    {
+        string query = """
+            SELECT first_name, last_name, passport_number, age, booking_id
+            FROM travelers
+            WHERE id = @id
+        """;
+       var parameters = new[]
+        {
+            new MySqlParameter("@id", id)
+        };
+
+        using var reader =
+            await MySqlHelper.ExecuteReaderAsync(
+                config.ConnectionString, query, parameters);
+if (reader.Read())
+        {
+            var result = new
+            {
+                FirstName = reader.GetString("first_name"),
+                LastName = reader.GetString("last_name"),
+                PassportNumber = reader.GetString("passport_number"),
+                Age = reader.GetInt32("age"),
+                BookingId = reader.GetInt32("booking_id"),
+                UserId = reader.GetInt32("user_id")
+            };
+            return Results.Ok(result);
+        }
+        return Results.NotFound(new { message = "Traveler not found." });
+    }
