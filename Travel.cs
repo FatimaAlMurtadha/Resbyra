5bb012d0be17b2fe2f67cae3d0ef006a48d39c5e
arbazshah52 <arbazexample.com>
2025-12-13 21:14:59 +0100

    deatil of travelling

--- Travel.cs
+++ Travel.cs
@@ -2,3 +2,8 @@ namespace server;
 
 using MySql.Data.MySqlClient;
 using Microsoft.AspNetCore.Http;
+
+class Travelers
+{
+ public record GetAll_Data(int Id,string FirstName,string LastName,string PassportNumber,int Age,int BookingId);
+ 
