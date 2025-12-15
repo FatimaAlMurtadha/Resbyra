7121d3f05e3ef6c32440c2c028ae75f8a9a672ae
FatimaAlmurtadha <sukaina77150@gmail.com>
2025-12-15 13:11:06 +0100

    Ading the class ROUTES

--- Program.cs
+++ Program.cs
@@ -164,6 +164,14 @@
     app.MapPost("/custom-cards-activities", CustomCardsActivities.Link);
     app.MapDelete("/custom-cards-activities/{cardId}/{activityId}", CustomCardsActivities.Unlink);
     app.MapGet("/custom-cards-activities/search", CustomCardsActivities.Search);
+
+
+    // CustomCardsHotelss ROUTES 
+    app.MapGet("/custom-cards-hotels/card/{cardId}", CustomCardHotels.ByCard);
+    app.MapGet("/custom-cards-hotels/hotel/{hotelId}", CustomCardHotels.ByHotel);
+    app.MapPost("/custom-cards-hotels", CustomCardHotels.Link);
+    app.MapDelete("/custom-cards-hotels/{cardId}/{hotelId}", CustomCardHotels.Unlink);
+    app.MapGet("/custom-cards-hotels/search", CustomCardHotels.Search);
     // special, reset db
     app.MapDelete("/db", db_reset_to_default);
 
